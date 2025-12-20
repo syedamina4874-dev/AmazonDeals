0dfac36031646d6607be430a27e173fe190f635e
HashGen <abechuphojaoff@gmail.com>
2025-12-20 17:32:05 +0530

    Update links.js

--- links.js
+++ links.js
@@ -1,4 +1,4 @@
-const AFFILIATE_TAG = "royalkrrish-21"; // Apna Tag Yahan Daalo
+const AFFILIATE_TAG = "royalkrrish00-21"; // Apna Tag Yahan Daalo
 
 const products = [
     "https://www.amazon.in/dp/B08L59X7",  
