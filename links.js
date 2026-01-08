419d41e9f5912721ac5f6c537ac1b51ba66b7ada
syedamina4874-dev <syedamina4874@gmail.com>
2026-01-08 11:38:16 +0500

    Update links.js

--- links.js
+++ links.js
@@ -1,17 +1,5 @@
 const AFFILIATE_TAG = "royalkrrish00-21"; // Apna Tag Yahan Daalo
 
 const products = [
-    "https://amzn.to/48NI9pI",
-    "https://www.amazon.in/dp/B0DTTZ86TV",
-    "https://amzn.to/491Lwbp",
-    "https://amzn.to/48OzQKr",
-    "https://amzn.to/4arvYAe",
-    "https://amzn.to/4qq0Tlf",
-    "https://amzn.to/4pXMDQH",
-    "https://amzn.to/3KVZjs8",
-    "https://amzn.to/3YBlKWy",
-    "https://amzn.to/44BTPcC",
-    "https://amzn.to/4pKbPKh",
-    "https://amzn.to/4qFkRbR",
-    // Aur links yahan comma (,) laga ke add karte jao
+    "
 ];
