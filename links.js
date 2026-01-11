ade5e277b7ff8491b7a160ace27d4a62ae6ffd16
syedamina4874-dev <syedamina4874@gmail.com>
2026-01-11 11:08:01 +0500

    Update links.js

--- links.js
+++ links.js
@@ -1,12 +1,5 @@
 const AFFILIATE_TAG = "girlyworld34-20"; 
 
 const products = [
-    "https://amzn.to/3LiJQTd",
-    "https://amzn.to/4bqemVC",
-    "https://amzn.to/4aTeIUD",
-    "https://amzn.to/4pAduBr",
-    "https://amzn.to/49wzozC",
-    "https://amzn.to/3Yx5NB2",
-    "https://amzn.to/452K2MZ",
-    "https://amzn.to/4qiV1KT",
+    "https://amzn.to/49Ku9NT",
 ];
